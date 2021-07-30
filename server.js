59e986705da1fb53d50199698993f74a3432aaec
Edison Li <47466335+edison4354@users.noreply.github.com>
2021-07-29 20:24:22 -0700

    Created Server File

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/restaurants/new', function (req, res) {
+  res.render('restaurants-new', {});
+});
+
+app.listen(3000);
